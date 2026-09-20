d0a368c16bb3734b5bf294a43a42718af84801d6
omvijaybhusari-coder <omvijaybhusari@gmail.com>
2026-09-20 17:28:50 +0530

    Time: 0 ms (100%), Space: 9.3 MB (50.66%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        char ans = 0;
+
+        for(char ch : s) {
+            ans = ans ^ ch;
+        }
+
+        for(char ch : t) {
+            ans = ans ^ ch;
+        }
+
+        return ans;
+    }
+};
